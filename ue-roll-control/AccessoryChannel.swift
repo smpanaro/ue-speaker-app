0d81ee85801068863826ff3572f5315c883dfa1f
Stephen Panaro <stephen.panaro@gmail.com>
2019-09-27 10:09:21 -0400

    Proof of concept checkpoint
    - App is capable of retrieving necessary data and turning speaker on.
    MACs are hardcoded for now though.

---
+++ ue-roll-control/AccessoryChannel.swift
@@ -0,0 +1,184 @@
+//
+//  AccessoryChannel.swift
+//  ue-roll-control
+//
+//  Created by Stephen Panaro on 9/27/19.
+//  Copyright © 2019 Stephen Panaro. All rights reserved.
+//
+
+import Foundation
+import ExternalAccessory
+
+
+class AccessoryChannel: NSObject {
+
+    let session: EASession
+    let queue: DispatchQueue
+
+    var isConfigured: Bool = false
+    var writeBuffer = Data(capacity: 0)
+
+    init(session: EASession) {
+        self.session = session
+        self.queue = DispatchQueue(label: "com.hedgemereapps.accessory-channel-queue", qos: .default, attributes: [], autoreleaseFrequency: .inherit, target: nil)
+        super.init()
+    }
+
+    func write(data: Data) {
+        configureIfNeeded()
+        queue.async { [weak self] in
+            self?.writeBuffer.append(data)
+            self?.handleWrite()
+        }
+    }
+
+    private func handleRead() {
+        guard let input = session.inputStream else { return }
+
+        var buffer = Data(capacity: 0)
+
+//        print("handling read, has bytes: \(input.hasBytesAvailable)")
+        while input.hasBytesAvailable {
+            do {
+                try buffer.append(input.read(maxLength: 100))
+            }
+            catch {
+                print("Error reading bytes.")
+                break
+            }
+        }
+
+        print("received \(buffer.count) bytes: \(buffer.hexEncodedString())")
+    }
+
+    private func handleWrite() {
+        guard let output = session.outputStream else { return }
+
+//        print("handling write, ouput has space: \(output.hasSpaceAvailable), buffer size: \(writeBuffer.count)")
+        while output.hasSpaceAvailable && writeBuffer.count > 0 {
+            do {
+                let bytesWritten = try output.write(data: writeBuffer)
+                writeBuffer.removeSubrange(0..<bytesWritten)
+            }
+            catch {
+                print("Error writing data.")
+                break
+            }
+        }
+    }
+
+
+    fileprivate func configureIfNeeded() {
+        if (isConfigured) {
+            return
+        }
+
+        session.inputStream?.delegate = self
+        session.inputStream?.schedule(in: .current, forMode: .default)
+        session.inputStream?.open()
+
+        session.outputStream?.delegate = self
+        session.outputStream?.schedule(in: .current, forMode: .default)
+        session.outputStream?.open()
+
+        isConfigured = true
+    }
+
+    deinit {
+        print("deinit")
+        session.inputStream?.close()
+        session.inputStream?.remove(from: .current, forMode: .default)
+
+        session.outputStream?.close()
+        session.outputStream?.remove(from: .current, forMode: .default)
+    }
+
+}
+
+extension AccessoryChannel: StreamDelegate {
+    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
+        print("got event: \(eventCode.toString())")
+        switch eventCode {
+        case .hasBytesAvailable:
+            queue.async { [weak self] in self?.handleRead() }
+            break
+        case .hasSpaceAvailable:
+            queue.async { [weak self] in self?.handleWrite() }
+            break
+        default:
+            break
+        }
+    }
+}
+
+extension Stream.Event {
+    func toString() -> String {
+        switch self {
+        case .endEncountered: return "endEncountered"
+        case .errorOccurred: return "errorOccurred"
+        case .hasBytesAvailable: return "hasBytesAvailable"
+        case .hasSpaceAvailable: return "hasSpaceAvailable"
+        case .openCompleted: return "openCompleted"
+        default:
+            return "unknown"
+        }
+    }
+}
+
+// https://forums.developer.apple.com/thread/116309
+extension InputStream {
+
+    func read(buffer: UnsafeMutableRawBufferPointer) throws -> Int {
+        // This check ensures that `baseAddress` will never be `nil`.
+        guard !buffer.isEmpty else { return 0 }
+        let bytesRead = self.read(buffer.baseAddress!.assumingMemoryBound(to: UInt8.self), maxLength: buffer.count)
+        if bytesRead < 0 {
+            throw self.guaranteedStreamError
+        }
+        return bytesRead
+    }
+
+    func read(maxLength: Int) throws -> Data {
+        precondition(maxLength >= 0)
+        var data = Data(repeating: 0, count: maxLength)
+        let bytesRead = try data.withUnsafeMutableBytes { buffer -> Int in
+            try self.read(buffer: buffer)
+        }
+        data.count = bytesRead
+        return data
+    }
+}
+
+extension OutputStream {
+
+    func write(buffer: UnsafeRawBufferPointer) throws -> Int {
+        // This check ensures that `baseAddress` will never be `nil`.
+        guard !buffer.isEmpty else { return 0 }
+        let bytesWritten = self.write(buffer.baseAddress!.assumingMemoryBound(to: UInt8.self), maxLength: buffer.count)
+        if bytesWritten < 0 {
+            throw self.guaranteedStreamError
+        }
+        return bytesWritten
+    }
+
+    func write(data: Data) throws -> Int {
+        return try data.withUnsafeBytes { buffer -> Int in
+            try self.write(buffer: buffer)
+        }
+    }
+}
+
+extension Stream {
+    var guaranteedStreamError: Error {
+        if let error = self.streamError {
+            return error
+        }
+        // If this fires, the stream read or write indicated an error but the
+        // stream didn’t record that error.  This is definitely a bug in the
+        // stream implementation, and we want to know about it in our Debug
+        // build. However, there’s no reason to crash the entire process in a
+        // Release build, so in that case we just return a dummy error.
+        assert(false)
+        return NSError(domain: NSPOSIXErrorDomain, code: Int(ENOTTY), userInfo: nil)
+    }
+}
--- ue-roll-control/ViewController.swift
+++ ue-roll-control/ViewController.swift
@@ -7,14 +7,193 @@
 //
 
 import UIKit
+import ExternalAccessory
+import CoreBluetooth
 
 class ViewController: UIViewController {
+
+    let powerOffCommand = Data(base64Encoded: "AgG2") // 0x02 01 B6 aka 438
+    let phoneMacAddressCommand = Data(base64Encoded: "AgGs") // 0x02 01 AC aka 428
+    let deviceMacAddressCommand = Data(base64Encoded: "AgGu") // 0x02 01 AE aka 430
+
+    let rollMacAddress = Data(base64Encoded: "wCiNAg8r")! // c0288d020f2b -- TODO: Save automatically.
+
+    let phoneMacAddressPlusOne = Data(base64Encoded: "zC23SLweAQ==")! // cc2db748bc1e01
+
+    let primaryUERollServiceUUID = CBUUID(string: "757ed3e4-1828-4a0c-8362-c229c3a6da72")
+    let powerOnCharacteristicUUID = CBUUID(string: "C6D6DC0D-07F5-47EF-9B59-630622B01FD3")
+
+    var accessoryManager: EAAccessoryManager!
+    var manager: CBCentralManager!
+
+    var roll: CBPeripheral!
+
+    var channel: AccessoryChannel?
+    var seenNames = Set<String>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        manager = CBCentralManager(delegate: self, queue: nil)
+
+        accessoryManager = EAAccessoryManager.shared()
+//        accessoryManager.registerForLocalNotifications()
+
+        if let acc = accessoryManager.connectedAccessories.first {
+            getMacAddress(accessory: acc)
+        }
+
+    }
+
+    func getMacAddress(accessory: EAAccessory) {
+        accessory.delegate = self
+        if let session = EASession(accessory: accessory, forProtocol: "com.logitech.ue.ueroll") {
+            channel = AccessoryChannel(session: session)
+            if let channel = channel,
+                let message = deviceMacAddressCommand {
+                channel.write(data: message)
+            }
+        }
+    }
+}
+
+extension Data {
+    struct HexEncodingOptions: OptionSet {
+        let rawValue: Int
+        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+    }
+
+    func hexEncodedString(options: HexEncodingOptions = []) -> String {
+        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
+        return map { String(format: format, $0) }.joined()
+    }
+}
+
+extension ViewController: EAAccessoryDelegate {
+    func accessoryDidDisconnect(_ accessory: EAAccessory) {
+        print("accessory disconnected: \(accessory)")
+    }
+}
+
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+          case .unknown:
+            print("central.state is .unknown")
+          case .resetting:
+            print("central.state is .resetting")
+          case .unsupported:
+            print("central.state is .unsupported")
+          case .unauthorized:
+            print("central.state is .unauthorized")
+          case .poweredOff:
+            print("central.state is .poweredOff")
+          case .poweredOn:
+            print("central.state is .poweredOn")
+            manager.scanForPeripherals(withServices: nil, options: nil)// [CBCentralManagerScanOptionSolicitedServiceUUIDsKey: [primaryUERollService]])
+            manager.registerForConnectionEvents(options: nil)
+        @unknown default:
+
+            print("central.state is unknowndefault")
+        }
     }
 
 
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if let value = advertisementData[CBAdvertisementDataManufacturerDataKey] as? Data,
+            value == rollMacAddress {
+            print("Found roll: \(peripheral)")
+            central.stopScan()
+
+            roll = peripheral
+            roll.delegate = self
+
+            central.connect(peripheral, options: nil)
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("did connect: \(peripheral)")
+
+        print(roll.discoverServices(nil))
+    }
 }
 
+extension ViewController: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        print("did discover services: \(String(describing: peripheral.services))")
+
+        if let primaryService = peripheral.services?.filter({ $0.uuid == primaryUERollServiceUUID }).first {
+            roll.discoverCharacteristics(nil, for: primaryService)
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        print("characteristics: \(String(describing: service.characteristics))")
+
+
+        if let powerCharacteristic = service.characteristics?.filter({ $0.uuid == powerOnCharacteristicUUID }).first {
+            roll.discoverDescriptors(for: powerCharacteristic)
+
+            print(powerCharacteristic.properties.toString())
+            peripheral.writeValue(phoneMacAddressPlusOne,
+                                  for: powerCharacteristic,
+                                  type: powerCharacteristic.properties.contains(.writeWithoutResponse) ? .withoutResponse : .withResponse)
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverDescriptorsFor characteristic: CBCharacteristic, error: Error?) {
+        print("discovered descriptors: \(String(describing: characteristic.descriptors))")
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        print("wrote value for characteristic: \(characteristic.uuid) with error: \(String(describing: error))")
+    }
+
+    func peripheralDidUpdateName(_ peripheral: CBPeripheral) {
+        print("did update name: \(String(describing: peripheral.name))")
+    }
+}
+
+extension OptionSet where RawValue: FixedWidthInteger {
+    func elements() -> AnySequence<Self> {
+        var remainingBits = rawValue
+        var bitMask: RawValue = 1
+        return AnySequence {
+            return AnyIterator {
+                while remainingBits != 0 {
+                    defer { bitMask = bitMask &* 2 }
+                    if remainingBits & bitMask != 0 {
+                        remainingBits = remainingBits & ~bitMask
+                        return Self(rawValue: bitMask)
+                    }
+                }
+                return nil
+            }
+        }
+    }
+}
+
+extension CBCharacteristicProperties {
+    func toString() -> String {
+        return elements().map({ $0.toSingleString() }).joined(separator: ", ")
+    }
+
+    func toSingleString() -> String{
+        switch self {
+        case .broadcast: return "broadcast"
+        case .read: return "read"
+        case .writeWithoutResponse: return "writeWithoutResponse"
+        case .write: return "write"
+        case .notify: return "notify"
+        case .indicate: return "indicate"
+        case .authenticatedSignedWrites: return "authenticatedSignedWrites"
+        case .extendedProperties: return "extendedProperties"
+        case .notifyEncryptionRequired: return "notifyEncryptionRequired"
+        case .indicateEncryptionRequired: return "indicateEncryptionRequired"
+        default:
+            fatalError()
+        }
+    }
+}
